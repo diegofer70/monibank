b9d8e181d24bb58959755369198538c731fbf970
Diego Araujo <ferreiradearaujod@gmail.com>
2024-08-25 23:26:55 -0300

    feat: descobrindo valores

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,7 +1,10 @@
 export default function ehUmCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
-    validaNumerosRepetidos(cpf);
-    validaNumerosDigito(cpf);
+    if (validaNumerosRepetidos(cpf) || !validaNumerosDigito(cpf) || !validaSegundoDigito(cpf)) {
+        console.log("Esse cpf não existe!");
+    } else {
+        console.log("Existe!");
+    }
     console.log(validaNumerosRepetidos(cpf));
     
 }
@@ -38,4 +41,22 @@ function validaNumerosDigito(cpf) {
     }
 
     return soma != cpf[9];
+}
+
+function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 1) {
+        soma = 0;
+    }
+
+    return soma != cpf[10];
 }
